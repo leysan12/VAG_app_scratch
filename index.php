e438b5d3c1b9ac24ae177e286fe6e6baa327d95a
leysan12 <lnu@munich-innovation.com>
2014-05-27 13:54:50 +0200

    rename vag_app_scratch

---
+++ index.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Vibroarthrography</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/> 
+<link rel="apple-touch-icon" href="favicon-114.png" />
+<meta name="apple-mobile-web-app-capable" content="yes" /><!-- hide top bar in mobile safari-->
+<!--<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> translucent top bar -->
+<!--<link rel="stylesheet" type="text/css" media="screen" href="style.css" />-->
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+
+	<div id="wrap">
+
+		<div id="main">
+		
+			<div class="header">
+				
+				<a class="left" href="?">Back</a>
+				<h1 class="title">Vibroarthrography</h1>
+				<a class="right" href="?">Next</a>
+				
+			</div><!--header-->
+			
+			<div class="content">
+			
+				<h2 class="title-c">Vibroarthrography Sign in</h2>
+				<div class="login-box">
+				<div class="box-white1">
+					<p>Mr. Some One<span class="detail">Details here</p>
+				</div><!--box-white-->
+				<span class="input-msg show"> 
+					<a class="IdURL" href="#">Forgot your login?</a>
+				</span>
+				<div class="login-box">
+				<div class="box-white1">
+					<p>Mr. Some One<span class="detail">Details here</p>
+				</div><!--box-white-->
+				</div>
+				<span class="input-msg show"> 
+					<a class="IdURL" href="#">Forgot your password?</a>
+				</span>
+							
+			</div><!--content-->
+		
+		</div><!--main-->
+	
+		<div id="sidebar">
+			
+			<div class="header">
+
+			</div><!--header-->
+			
+			<div class="content">
+				
+				<ul class="nav1">
+					<li><p class="txt">Sign in with your Apple ID to access Apple Support. If you don't have an Apple ID, you can create one now.</p></li>
+				</ul>
+
+			</div><!--content-->
+			
+		</div><!--sidebar-->
+	
+		
+		
+
+	</div><!--wrap-->
+
+
+<!--<script type="text/javascript" src="script.js">-->
+</body>
+</html>
